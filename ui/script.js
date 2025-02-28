0d7092681af15f50009e344b78944bf13f51bf34
BeereMgM <80534799+BeereMgM@users.noreply.github.com>
2025-02-28 23:51:28 +0100

    ui

---
+++ ui/script.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("message", function (event) {
+
+        if (event.data.action === "openUI") {
+            const container = document.getElementById("seat-container");
+            const uiContainer = document.getElementById("ui-container");
+
+            if (!container || !uiContainer) {
+                console.error("UI-Container oder Seat-Container nicht gefunden!");
+                return;
+            }
+
+            container.innerHTML = "";
+
+            let rowDiv = null; 
+            for (let i = 1; i <= event.data.seats; i++) {
+                if (i % 2 !== 0) {
+                    rowDiv = document.createElement("div");
+                    rowDiv.classList.add("seat-row");
+                    container.appendChild(rowDiv);
+                }
+
+                let seatDiv = document.createElement("div");
+                seatDiv.classList.add("seat");
+                seatDiv.textContent = i;
+                seatDiv.onclick = () => {
+                    fetch(`https://car_door/selectSeat`, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ seat: i, vehicle: event.data.vehicle })
+                    });
+                    
+                    const uiContainer = document.getElementById("ui-container");
+                    if (uiContainer) {
+                        uiContainer.style.display = "none";
+                    }
+                } 
+                
+    
+
+                if (rowDiv) {
+                    rowDiv.appendChild(seatDiv);
+                }
+            }
+
+            uiContainer.style.display = "block"; 
+        }
+    });
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+            fetch(`https://car_door/closeUI`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" }
+            });
+
+            const uiContainer = document.getElementById("ui-container");
+            if (uiContainer) {
+                uiContainer.style.display = "none";
+            }
+        }
+    });
+});
